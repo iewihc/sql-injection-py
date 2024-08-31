8e9ac6d8a5e21667e8ff652b1ee0c978f6fe047d
iewihc <wei@miniasp.com>
2024-08-31 15:47:44 +1000

    add more vul

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
-from flask import Flask
-from flask import request
-from flask import config
-from flask import render_template_string
+from flask import Flask, request, render_template_string
+import sqlite3
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "flag{SSTI_123456}"
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/users')
+def get_user():
+    username = request.args.get('username')
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    # 不安全的查询 - SQL 注入漏洞
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    cursor.execute(query)
+    user = cursor.fetchone()
+    conn.close()
+    return str(user)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -22,4 +34,4 @@ def page_not_found(e):
     return render_template_string(template), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
